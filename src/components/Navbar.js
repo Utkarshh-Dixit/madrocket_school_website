70db2315081a8a595dcce84f2a43d0ab4502a7d6
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-07-24 01:32:37 +0530

    Styling nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,65 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav className=" bg-blue-500 p-4 text-white">
-    <div className="container mx-auto flex justify-between">
-      <div className="font-bold">Springdale Public School</div>
-      <div>
-        <Link to="/" className="mr-4">
+  <nav className="bg-blue-600 p-4 text-white shadow-lg">
+    <div className="container mx-auto flex justify-between items-center">
+      <div className="text-2xl font-bold">Springdale Public School</div>
+      <div className="space-x-6">
+        <Link
+          to="/"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Home
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/about" className="mr-4">
+        <Link
+          to="/about"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           About Us
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/academics" className="mr-4">
+        <Link
+          to="/academics"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Academics
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/admissions" className="mr-4">
+        <Link
+          to="/admissions"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Admissions
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/faculty" className="mr-4">
+        <Link
+          to="/faculty"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Faculty
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/students" className="mr-4">
+        <Link
+          to="/students"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Students
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/gallery" className="mr-4">
+        <Link
+          to="/gallery"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Gallery
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
-        <Link to="/contact" className="mr-4">
+        <Link
+          to="/contact"
+          className="relative group inline-block px-3 py-2 transition-transform duration-300 ease-in-out"
+        >
           Contact Us
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </Link>
       </div>
     </div>
